b36f672420766901998362104d857a4aecd9bca0
Jing Jing Zhang <92764807+jjzhang329@users.noreply.github.com>
2024-08-12 16:03:17 -0500

    neetcode

---
+++ leetcode/49. Group Anagrams.py
@@ -0,0 +1,15 @@
+# using array to form a unique key
+# anagram words will have same key cause they have same chars
+# result = { key: [words] },return result's values
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        result = defaultdict(list)
+        for s in strs:
+            count = [0] * 26
+            for c in s:
+                count[ord(c) - ord('a')] += 1
+            key = tuple(count)
+            result[key].append(s)
+        
+        return result.values()
