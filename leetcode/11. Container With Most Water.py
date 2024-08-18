09e6a161ce7ae9aa2f1a647f864ad960db3ca8e0
Jing Jing Zhang <92764807+jjzhang329@users.noreply.github.com>
2024-08-18 13:50:15 -0500

    two pointer

--- leetcode/11. Container With Most Water.py
+++ leetcode/11. Container With Most Water.py
@@ -1,3 +1,18 @@
+# two pointer, always use the higher bar
+
 class Solution:
     def maxArea(self, height: List[int]) -> int:
-        
+        result= 0 
+        left, right = 0, len(height)-1
+
+        while left < right:
+            width = right - left 
+            area  = min(height[right], height[left]) * width
+            if height[right] > height[left]:
+                left += 1
+            else:
+                right -= 1
+
+            result = max(result, area)
+
+        return result
