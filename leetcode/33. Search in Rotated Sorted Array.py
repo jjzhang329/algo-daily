a0b945c65eaaacbe2d162bedd49db40dac4e16e9
Jing Jing Zhang <92764807+jjzhang329@users.noreply.github.com>
2024-08-26 22:10:08 -0500

    binary search

---
+++ leetcode/33. Search in Rotated Sorted Array.py
@@ -0,0 +1,25 @@
+# first determine if left array is sorted or right is sorted
+# then compare the mid with edge
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        left, right = 0, len(nums)-1
+
+
+        while left <= right:
+            mid = (left + right) // 2
+
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] >= nums[left]:
+                if target < nums[mid] and target >= nums[left]:
+                    right = mid - 1
+                else:
+                    left = mid + 1
+            else:
+                if target <= nums[right] and target > nums[mid]:
+                    left = mid + 1
+                else:
+                    right = mid - 1
+        
+        return -1
