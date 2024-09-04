639bd3ffbc79910b8d00062c6ab0c9b783e2a47e
Jing Jing Zhang <92764807+jjzhang329@users.noreply.github.com>
2024-09-03 20:47:17 -0500

    backtracking

---
+++ leetcode/78.Subsets.py
@@ -0,0 +1,22 @@
+#backtracking
+#子集问题，画出树图的话，子集问题是收集每一个节点的结果
+
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        res = []
+
+        subset = []
+
+        def dfs(i):
+            if i >= len(nums):
+                res.append(subset.copy())
+                return
+            # decision to include nums[i]
+            subset.append(nums[i])
+            dfs(i + 1)
+            # decision NOT to include nums[i]
+            subset.pop()
+            dfs(i + 1)
+
+        dfs(0)
+        return res
